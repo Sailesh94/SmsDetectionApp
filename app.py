7bc4975d78219cde5d272a3de42aa90e101661a4
Sailesh94 <71598275+Sailesh94@users.noreply.github.com>
2020-12-04 22:46:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import nltk
+from nltk.corpus import stopwords
+import string
+import pickle
+app = Flask(__name__)
+model = pickle.load(open('SmsDetectorModel.mdl' , "rb"))
+TfIdfObject = pickle.load(open('TFIDobject.obj','rb'))
+PreprocessedText =pickle.load(open('PreprocessedText.txt','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    SmsInput = input("Enter SMS: ")
+def textProcessor(featureRecord):
+    removePunctuation = [word for word in featureRecord if word not in string.punctuation]
+    sentences = "".join(removePunctuation)
+    words = sentences.split(" ")
+    wordNormalize = [word.lower() for word in words]
+    finalWords = [word for word in wordNormalize if word not in stopwords.words("english")]
+    return finalWords
+    preproccessedtext = textProcessor(SmsInput)
+    bowfeatures = PreprocessedText.transform(preproccessedtext)
+    tfIdffeature = TfIdfObject.transform(bowfeatures)
+    predLabel = model.predict(tfIdffeature)[0]  
+
+    return render_template('index.html', prediction_text='{} is a {} SMS.'.format(SmsInput,predLabel))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
